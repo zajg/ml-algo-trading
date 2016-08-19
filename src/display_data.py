f3be98a4cdc50b69d53e2a6885b788d1491b87f4
grzesiekzajac <grzesiek.zajac1@gmail.com>
2016-08-19 23:20:45 +0200

    Improved ploting layout

--- src/display_data.py
+++ src/display_data.py
@@ -5,6 +5,8 @@ import matplotlib.ticker as mticker
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import style
+style.use("ggplot")
 
 
 def displayRawData():
@@ -12,16 +14,23 @@ def displayRawData():
 								unpack=True, 
 								delimiter=',', 
 								converters={0:mdates.strpdate2num('%Y%m%d%H%M%S')})
+	
 	fig = plt.figure(figsize=(10,7))
 	ax1 = plt.subplot2grid((40,40), (0,0), rowspan=40, colspan=40)
-
+	
 	ax1.plot(date,bid)
 	ax1.plot(date,ask)
+	
+	ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%Y\n%H:%M:%S'))
 
-	ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%Y %H:%M:%S'))
+	#for label in ax1.xaxis.get_ticklabels():
+    #		label.set_rotation(90)
 
+	plt.subplots_adjust(bottom=.23)
+	plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
 	plt.grid(True)
 	plt.show()
+
 
 if __name__ == "__main__":
 	displayRawData()
