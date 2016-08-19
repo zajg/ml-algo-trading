237624dcddc47a9d4093ab4ac5ee5f6a1ea014de
grzesiekzajac <grzesiek.zajac1@gmail.com>
2016-08-19 22:59:21 +0200

    Ploting exchange rates

---
+++ src/display_data.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python 
+
+import matplotlib
+import matplotlib.ticker as mticker
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def displayRawData():
+	date,bid,ask = np.loadtxt('../data/GBPUSD1d.txt', 
+								unpack=True, 
+								delimiter=',', 
+								converters={0:mdates.strpdate2num('%Y%m%d%H%M%S')})
+	fig = plt.figure(figsize=(10,7))
+	ax1 = plt.subplot2grid((40,40), (0,0), rowspan=40, colspan=40)
+
+	ax1.plot(date,bid)
+	ax1.plot(date,ask)
+
+	ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%Y %H:%M:%S'))
+
+	plt.grid(True)
+	plt.show()
+
+if __name__ == "__main__":
+	displayRawData()
